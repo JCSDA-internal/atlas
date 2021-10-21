097464dd6390ab461dc981e938c2be27563199f8
MO-marcomilan <marco.milan@metoffice.gov.uk>
2021-10-21 08:47:37 +0100

    Test also var_ratio=1

--- src/tests/grid/test_stretchedLAM.cc
+++ src/tests/grid/test_stretchedLAM.cc
@@ -37,22 +37,22 @@ const double lat_LAM_str[] = {
                                 8.66534677777778,  9.386306244003876,  10.18456345460838,  11.06840589531815
     };
 
-const double lon_LAM_reg[] = {349.433,    350.085,    350.736,    351.387,
-                              352.038,    352.689,    353.34,    353.992,
-                              354.643,    355.294,    355.945,   356.596,
-                              357.247,    357.898,    358.55,   359.201,
-                              359.852,    360.503,    361.154,   361.805,
-                              362.456,    363.108,    363.759,   364.41,
-                              365.061,    365.712,    366.363,   367.015,
-                              367.666,    368.317};
+const double lon_LAM_reg[] = {349.4335,  350.084648275862,  350.7357965517241, 351.3869448275862,
+                              352.0380931034483,  352.6892413793103,  353.3403896551724,  353.9915379310345,
+                              354.6426862068965,  355.2938344827586,  355.9449827586207,  356.5961310344828,
+                              357.2472793103448,  357.8984275862069,  358.5495758620689,  359.200724137931,
+                              359.8518724137931,  360.5030206896552,  361.1541689655172,  361.8053172413793,
+                              362.4564655172414,  363.1076137931034,  363.7587620689655,  364.4099103448276,
+                              365.0610586206897,  365.7122068965517,  366.3633551724138,  367.0145034482759,
+                              367.6656517241379,  368.3168};
 
-const double lat_LAM_reg[] = {-6.9622,    -6.31105,    -5.6599,    -5.00876,
-                              -4.35761,    -3.70646,    -3.05531,    -2.40416,
-                              -1.75302,    -1.10187,    -0.450722,   0.200426,
-                              0.851574,    1.50272,    2.15387,    2.80502,
-                              3.45616,    4.10731,    4.75846,    5.40961,
-                              6.06076,    6.7119,    7.36305,    8.0142,
-                              8.66535,    9.31649,    9.96764,    10.6188
+const double lat_LAM_reg[] = {-6.962199,  -6.311051259259259,  -5.659903518518519,  -5.008755777777777,
+                              -4.357608037037037,  -3.706460296296296,  -3.055312555555555,  -2.404164814814814,
+                              -1.753017074074074,  -1.101869333333333,  -0.4507215925925925,  0.2004261481481482,
+                              0.8515738888888897,  1.50272162962963,  2.153869370370371,  2.805017111111111,
+                              3.456164851851852,  4.107312592592594,  4.758460333333334,  5.409608074074075,
+                              6.060755814814815,  6.711903555555557,  7.363051296296296,  8.014199037037038,
+                              8.66534677777778,  9.316494518518519,  9.967642259259259,  10.61879
 };
 
 };
@@ -86,6 +86,8 @@ CASE( "LAMstretch" ) {
                                                  Config( "y_reg_end", 8.66534677777778 ) | Config( "startx", 349.4335) |
                                                  Config( "starty",  -6.962199) | Config( "endx", 368.3168) | Config( "endy", 10.61879) |
                                                  Config( "north_pole", {0.0, 90.0} ) );
+
+
 
 
     std::cout<< "projection name " << proj.type() <<std::endl;
@@ -172,7 +174,59 @@ CASE( "LAMstretch" ) {
             EXPECT_APPROX_EQ( ll1lat, ll2lat, 1.e-10 );
         }
     }
+
+    /*
+    atlas::util::Config proj_grid_config;
+    proj_grid_config.set( "type", "stretch" );
+    proj_grid_config.set( "delta_low",  0.6545448275862076);
+    proj_grid_config.set( "delta_hi", 0.6545448275862076 );
+    proj_grid_config.set( "var_ratio", 1.0 );
+    proj_grid_config.set( "x_reg_start", 351.3869448275862 );
+    proj_grid_config.set( "y_reg_start", -5.008755777777777 );
+    proj_grid_config.set( "x_reg_end", 366.3633551724138 );
+    proj_grid_config.set( "y_reg_end", 8.66534677777778 );
+    proj_grid_config.set( "startx", 349.4335 );
+    proj_grid_config.set( "starty", -6.962199 );
+    proj_grid_config.set( "endx", 368.3168 );
+    proj_grid_config.set( "endy", 10.61879 );
+    proj_grid_config.set( "north_pole", {0.0, 90.0} );
+
+    */
+
+    auto proj_reg = Projection( "stretch", Config( "delta_low", 0.6545448275862076 ) | Config( "delta_hi", 0.6511482758621128 ) |
+                                                 Config( "var_ratio", 1.0 ) | Config( "x_reg_start", 351.3869448275862 ) |
+                                                 Config( "y_reg_start", -5.008755777777777 ) | Config( "x_reg_end", 366.3633551724138 ) |
+                                                 Config( "y_reg_end", 8.66534677777778 ) | Config( "startx", 349.4335) |
+                                                 Config( "starty",  -6.962199) | Config( "endx", 368.3168) | Config( "endy", 10.61879) |
+                                                 Config( "north_pole", {0.0, 90.0} ) );
+    //definition of stretched grid
+    auto grid_reg = StructuredGrid(XS, YS, proj_reg );
+
+
+    std::cout<< "projection name " << proj.type() <<std::endl;
+
+    //definition of stretched grid
+    //auto grid_reg = StructuredGrid(proj_grid_config );
+
+
+    //check over regular grid points stretched using new atlas object and check using look-up table
+    for (atlas::idx_t j = nodes_reg.j_begin(); j < nodes_reg.j_end(); ++j) {
+        for (atlas::idx_t i = nodes_reg.i_begin(j); i < nodes_reg.i_end(j); ++i) {
+            auto ll1lon = lon_LAM_reg[i];
+            auto ll1lat = lat_LAM_reg[j];
+            auto ll2 = grid_reg.lonlat( i, j );
+            auto ll2lon = ll2.lon();
+            auto ll2lat = ll2.lat();
+            //std::cout << " i lon: " << i << " j lat: " << j << std::endl;
+            //std::cout << "lon from array ll1: " << ll1lon << " lon computed ll2: " << ll2lon << std::endl;
+            EXPECT_APPROX_EQ( ll1lon , ll2lon, 1.e-10 );
+            EXPECT_APPROX_EQ( ll1lat, ll2lat, 1.e-10 );
+        }
+    }
+
 }
+
+
 
 
 }  // namespace test

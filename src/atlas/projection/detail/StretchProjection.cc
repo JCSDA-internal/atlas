123139a3a884282160f53efdac6d2f6acf28ab45
MO-marcomilan <marco.milan@metoffice.gov.uk>
2021-10-27 13:24:58 +0100

    Delete log files, change Stretch class with better comments and review dependencies

--- src/atlas/projection/detail/StretchProjection.cc
+++ src/atlas/projection/detail/StretchProjection.cc
@@ -1,11 +1,8 @@
 /*
- * (C) Copyright 2013 ECMWF.
+ * (C) Crown Copyright 2021 Met Office
  *
  * This software is licensed under the terms of the Apache Licence Version 2.0
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
- * In applying this licence, ECMWF does not waive the privileges and immunities
- * granted to it by virtue of its status as an intergovernmental organisation
- * nor does it submit to any jurisdiction.
  */
 
 #include <cmath>
@@ -20,39 +17,26 @@
 #include "atlas/runtime/Exception.h"
 #include "atlas/util/Config.h"
 #include "atlas/util/Constants.h"
-#include "atlas/util/CoordinateEnums.h"
-#include "atlas/util/Earth.h"
-//#include "atlas/util/NormaliseLongitude.h"
-#include "atlas/util/Point.h"
 #include "atlas/grid.h"
 #include "atlas/grid/Grid.h"
 #include "atlas/functionspace/StructuredColumns.h"
 #include "eckit/testing/Test.h"
-#include <iomanip> // for setprecision
+#include <iomanip> ///< for setprecision
 
 
-/*
-Projection for LAM stretching
-
-The origin of the xy-system is at (lon0,0)
-
+/**
+* Projection for LAM stretching
+*
+* The origin of the xy-system is at (lon,lat) = (0,0)
+*
 */
-
-namespace {
-static constexpr double D2R( const double x ) {
-    return atlas::util::Constants::degreesToRadians() * x;
-}
-static constexpr double R2D( const double x ) {
-    return atlas::util::Constants::radiansToDegrees() * x;
-}
-}  // namespace
 
 namespace atlas {
 namespace projection {
 namespace detail {
 
 
-//specification parameters
+// specification parameters
 template <typename Rotation>
 typename StretchLAM<Rotation>::Spec StretchLAM<Rotation>::spec() const {
 
@@ -70,10 +54,6 @@ typename StretchLAM<Rotation>::Spec StretchLAM<Rotation>::spec() const {
     proj_st.set( "endy", endy_ ); //domain endy
 
     return proj_st;
-    //call function for stretch
-
-    //double st_lon, st_lat;
-    //return PointXY(st_lon, st_lat);
 }
 
 // constructors
@@ -106,7 +86,7 @@ void StretchLAM<Rotation>::checkvalue(const double & epsilon,
              throw eckit::BadValue(err_message, Here());
           }
 }
-//lamphi, true, nx_, n_stretched_)
+// General stretch from a point in regular grid to the stretched grid
 template<typename Rotation>
 double StretchLAM<Rotation>::general_stretch(double& lamphi, const bool& L_long,
                                              int& n_int, const int& n_stretched_) const {
@@ -116,44 +96,33 @@ double StretchLAM<Rotation>::general_stretch(double& lamphi, const bool& L_long,
        double lamphi_end;
        double point = lamphi;
 
-       if ((n_int > 0 ) && (var_ratio_>1)){
+       if ((n_int > 0 ) && (var_ratio_ > 1)){
 
           double delta_dist;
           double delta_add;
 
-          //input number of points, output intervals
+          // input number of points, output intervals
           n_int -= 1;
           high_size = (n_int - n_stretched_ ) * delta_high_;
-          //std::cout << "high_size: " << high_size << std::endl;
 
-          //number of new internal regular grid in integer
+          // number of new internal regular grid in integer
           int ints_high = (high_size + 0.0001) / delta_high_;
 
-          //number of variable grid
+          // number of variable grid
           int var_ints = (n_int - ints_high)/2.;
-          //compute ratio
+          // compute ratio
           double var_ints_f = (n_int - ints_high)/2.;
           double logr = log(var_ratio_);
           double log_ratio = (var_ints_f - 0.5) * logr;
-          //double new_ratio = exp(log_ratio / (double) var_ints);
-          //double new_ratio = exp(log_ratio / var_ints_f);
           double new_ratio = exp(log_ratio / var_ints);
-          //double rpowerY = pow(new_ratio, var_ints);
-          /*
-          std::cout << std::setprecision(15) << "MARCO ints_high: " << ints_high << std::endl;
-          std::cout << std::setprecision(15) << "MARCO var_ints: " << var_ints << std::endl;
-          std::cout << std::setprecision(15) << "MARCO var_ints_f: " << var_ints_f << std::endl;
-          std::cout << std::setprecision(15) << "MARCO logr: " << logr << std::endl;
-          std::cout << std::setprecision(15) << "MARCO log_ratio: " << log_ratio << std::endl;
-          std::cout << std::setprecision(15) << "MARCO new_ratio: " << new_ratio << std::endl;
-          */
-          //rim To implement
-          //int ints_low = rim_width;
-          // total intervals not used here but maybe for later
-          //int intervals = ints_high + 2* (var_ints + ints_low);
-          // print('intervals : ', intervals, 'n_int (pointnew) : ', intervals + 1)
 
-          //INTERNAL REGULAR GRID the point is mapped to the same point
+          /** rim To implement
+          * int ints_low = rim_width;
+          * total intervals not used here but maybe for later
+          * int intervals = ints_high + 2* (var_ints + ints_low);
+          */
+
+          // INTERNAL REGULAR GRID the point is mapped to the same point
           if (L_long ) {
               lamphi_start = x_reg_start_;
           } else {
@@ -164,10 +133,10 @@ double StretchLAM<Rotation>::general_stretch(double& lamphi, const bool& L_long,
           if ((point >= lamphi_start) && (point <= lamphi_end)){
                    lamphi = point;
           }
-          // VERY IMPORTANT
-          //Stat variable res 'STRETCHED'
-          //distance from the regular grid and stretch internal grid delta_high
-
+          /* VERY IMPORTANT
+          * Stat variable res 'STRETCHED'
+          * distance from the regular grid and stretch internal grid delta_high
+          */
 
           if (point < lamphi_start) {
              delta_dist = abs(lamphi_start - point);
@@ -179,19 +148,19 @@ double StretchLAM<Rotation>::general_stretch(double& lamphi, const bool& L_long,
              // number of high resolution points intervals
              // delta_dist is the distance of the actual point to the internal regular grid
              int n_high = delta_dist / delta_high_;
-             //part remaining, use modulo
+             // part remaining, use modulo
              double p_rem = std:: fmod(delta_dist , delta_high_);
-             //computation of the new stretched delta integer part
+             // computation of the new stretched delta integer part
              double delta = delta_high_;
-             //initialization if is not using the cycle (first interval)
+             // initialization if is not using the cycle (first interval)
              double delta_last = delta;
 
-             //using different in delta
+             // using difference in delta
              for (int i = 0; i < n_high; i+=1){
                 delta_last = delta * new_ratio;
                 delta_add = delta_last - delta_high_;
                 delta = delta_last;
-                //recomputation of point for every interval
+                // recomputation of point for every interval
                 if (point > lamphi_start){
                    point = point + delta_add;
                 }else {
@@ -199,7 +168,7 @@ double StretchLAM<Rotation>::general_stretch(double& lamphi, const bool& L_long,
                 }
              }
 
-             //last part, delta last from the cycle before
+             // last part, delta last from the cycle before
              double delta_r = p_rem * pow(new_ratio, (n_high+1));
              double delta_addr = delta_r - p_rem;
              if (point > lamphi_start){
@@ -213,13 +182,17 @@ double StretchLAM<Rotation>::general_stretch(double& lamphi, const bool& L_long,
           }
           //Section 5: Reset lat/lons
 
-          // If longitude domain crosses the meridian (0 degrees) then
-          // add 360 degrees to points East of the meridian
+          /*
+          * If longitude domain crosses the meridian (0 degrees) then
+          * add 360 degrees to points East of the meridian
+          */
           if (L_long){
               lamphi = (lamphi < 180) ? lamphi + 360.0 : lamphi ;
           }
 
-       } else { //  var_ratio = 1.0   regular grid //line 124 python function
+       }
+       //  var_ratio = 1.0   regular grid
+       else {
            // Section 6: regular grids
             lamphi = point;
             if (L_long){
@@ -232,88 +205,14 @@ double StretchLAM<Rotation>::general_stretch(double& lamphi, const bool& L_long,
 
 }
 
-/*
-template<typename Rotation>
-//atlas::PointXY StretchLAM<Rotation>::stretch_LAM_gen(atlas::PointXY& xyNotStretched) {
-void StretchLAM<Rotation>::stretch_LAM_gen(double crd[]) {
-    double lam_hires_size;
-    double phi_hires_size;
-    double lambda_start;
-    double phi_start;
-    double add_xf_;
-    double add_yf_;
-    double check_x;
-    double check_y;
-    //double grid_ratio;
-    int rim_size;
-    //double lamphi;
-    double epsilon = 0.01;
-    double st_lon, st_lat;
-
-
-    double deltax_all = (endx_ - startx_);
-    double deltay_all = (endy_ - starty_);
-
-    if (var_ratio_ == 1) {
-           lam_hires_size = deltax_all;
-           phi_hires_size = deltay_all;
-           lambda_start = x_reg_start_ ;
-           phi_start = y_reg_start_ ;
-         } else {
-              lam_hires_size = x_reg_end_ - x_reg_start_;
-              phi_hires_size = y_reg_end_ - y_reg_start_;
-              //add check for start of the new regular LAM grid x and y
-              //in the middle of the previousregular grid
-
-              add_xf_ = (deltax_all - lam_hires_size) / 2.;
-              add_yf_ = (deltay_all - phi_hires_size) / 2.;
-              int add_x = (deltax_all - lam_hires_size) / delta_high_;
-              int add_y = (deltay_all - phi_hires_size) / delta_high_;
-              lambda_start = x_reg_start_;
-              phi_start = y_reg_start_;
-              check_x = xmin_ + add_xf_ - lambda_start;
-              check_y = ymin_ + add_yf_ - phi_start;
-              double check_st;
-              check_st = add_x - add_y;
-              checkvalue(epsilon, check_x);
-              checkvalue(epsilon, check_y);
-              checkvalue(epsilon, check_st);
-
-              n_stretched_ = add_x;
-              nx_ = deltax_all / delta_high_;
-              ny_ = deltay_all / delta_high_;
-
-         }
-
-    //grid_ratio = delta_low_ / delta_high_;
-    rim_size = 0;
-    //lamphi = xyNotStretched(0);
-    //lamphi = crd[0];
-    //st_lon = general_stretch(lamphi, true, nx_, n_stretched_);
-    crd[0] = general_stretch(crd[0], true, nx_, n_stretched_);
-
-    //lamphi = xyNotStretched(1);
-    //lamphi = crd[1];
-    //st_lat = general_stretch(lamphi, false, ny_, n_stretched_);
-    crd[1] = general_stretch(crd[1], false, ny_, n_stretched_);
-
-    //return PointXY(st_lon, st_lat);
-
-
-}
-
-*/
-
-
-//xy unstretched
-//Do only unrotation!!!
+//xy unstretched, only unrotation
 template <typename Rotation>
 void StretchLAM<Rotation>::lonlat2xy( double crd[] ) const {
 
     //unrotate
     rotation_.rotate( crd );
 
-    //PUT the unstretch, I don't have it nows
+    // PUT the unstretch, I don't have it nows
 
 }
 
@@ -321,11 +220,12 @@ void StretchLAM<Rotation>::lonlat2xy( double crd[] ) const {
 template <typename Rotation>
 void StretchLAM<Rotation>::xy2lonlat( double crd[] ) const {
 
-    //PUT here the stretch for a point that come input
-    //give number of power as input,work it out using as from the start of regular grid.
-    //atlas::PointXY unstretchedXY = crd;
-    //atlas::PointXY stretchedXY = stretch_LAM_gen(unstretchedXY);
-    //stretch_LAM_gen(crd[]);
+    /** PUT here the stretch for a point that come input
+    * give number of power as input,work it out using as from the start of regular grid.
+    * atlas::PointXY unstretchedXY = crd;
+    * atlas::PointXY stretchedXY = stretch_LAM_gen(unstretchedXY);
+    * stretch_LAM_gen(crd[]);
+    */
 
     double lam_hires_size;
     double phi_hires_size;
@@ -335,9 +235,7 @@ void StretchLAM<Rotation>::xy2lonlat( double crd[] ) const {
     double add_yf_;
     double check_x;
     double check_y;
-    //double grid_ratio;
     int rim_size;
-    //double lamphi;
     double epsilon = 0.01;
     int nx_, ny_, n_stretched_;
 
@@ -353,8 +251,10 @@ void StretchLAM<Rotation>::xy2lonlat( double crd[] ) const {
          } else {
               lam_hires_size = x_reg_end_ - x_reg_start_;
               phi_hires_size = y_reg_end_ - y_reg_start_;
-              //add check for start of the new regular LAM grid x and y
-              //in the middle of the previousregular grid
+
+              /** check for start of the new regular LAM grid x and y
+              * in the middle of the previous regular grid
+              */
 
               add_xf_ = (deltax_all - lam_hires_size) / 2.;
               add_yf_ = (deltay_all - phi_hires_size) / 2.;
@@ -366,7 +266,7 @@ void StretchLAM<Rotation>::xy2lonlat( double crd[] ) const {
               check_x = startx_ + add_xf_ - lambda_start;
               check_y = starty_ + add_yf_ - phi_start;
               double check_st;
-              //std::cout << "MARCO check stretched all x: " << add_x << " y:" << add_y << std::endl;
+
               check_st = add_x - add_y;
               checkvalue(epsilon, check_x);
               checkvalue(epsilon, check_y);
@@ -375,29 +275,15 @@ void StretchLAM<Rotation>::xy2lonlat( double crd[] ) const {
               n_stretched_ = add_x;
               nx_ = ((deltax_all + 0.0001) / delta_high_) + 1;
               ny_ = ((deltay_all + 0.0001) / delta_high_ ) + 1;
-              //std::cout << "MARCO deltay_all : " << deltay_all << " delta_high:" << delta_high_ << std::endl;
-              //std::cout << "MARCO endy : " << endy_ << " starty:" << starty_ << std::endl;
-              //std::cout << "MARCO check all poits x: " << nx_ << " y:" << ny_ << std::endl;
-
          }
 
-    //grid_ratio = delta_low_ / delta_high_;
     rim_size = 0;
-    //lamphi = xyNotStretched(0);
-    //lamphi = crd[0];
-    //st_lon = general_stretch(lamphi, true, nx_, n_stretched_);
     crd[0] = general_stretch(crd[0], true, nx_, n_stretched_);
-
-    //lamphi = xyNotStretched(1);
-    //lamphi = crd[1];
-    //st_lat = general_stretch(lamphi, false, ny_, n_stretched_);
     crd[1] = general_stretch(crd[1], false, ny_, n_stretched_);
 
     // rotate
     rotation_.unrotate( crd );
 }
-
-
 
 template <typename Rotation>
 ProjectionImpl::Jacobian StretchLAM<Rotation>::jacobian( const PointLonLat& ) const {
--- src/atlas/projection/detail/StretchProjection.h
+++ src/atlas/projection/detail/StretchProjection.h
@@ -1,11 +1,8 @@
 /*
- * (C) Copyright 2013 ECMWF.
+ * (C) Crown Copyright 2021 Met Office
  *
  * This software is licensed under the terms of the Apache Licence Version 2.0
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
- * In applying this licence, ECMWF does not waive the privileges and immunities
- * granted to it by virtue of its status as an intergovernmental organisation
- * nor does it submit to any jurisdiction.
  */
 
 #pragma once
@@ -13,20 +10,15 @@
 #include "atlas/domain.h"
 #include "atlas/projection/detail/ProjectionImpl.h"
 #include "atlas/util/NormaliseLongitude.h"
-#include "atlas/util/Point.h"
-
-
 
 namespace atlas {
 namespace projection {
 namespace detail {
 
-
 template <typename Rotation>
 class StretchLAM final : public ProjectionImpl {
 public:
     using Spec = ProjectionImpl::Spec;
-    atlas::PointXY point_reg;
 
     // constructor uses parametrisation and point to stretch
     StretchLAM(const eckit::Parametrisation& );
@@ -34,10 +26,9 @@ public:
     static std::string static_type() { return Rotation::typePrefix() + "stretch"; }
     std::string type() const override { return static_type(); }
 
-    // projection and inverse projection, I will use the POINTXY in the function
+    // projection and inverse projection
     void xy2lonlat( double crd[] ) const override;
     void lonlat2xy( double crd[] ) const override;
-
 
     // specification for stretching
     Spec spec() const override;
@@ -49,47 +40,31 @@ public:
     // NOT for the moment
     Jacobian jacobian( const PointLonLat& ) const override;
 
-    bool strictlyRegional() const override { return true; }  // Stretch projection cannot be used for global grids
+    bool strictlyRegional() const override { return true; }  ///< Stretch projection cannot be used for global grids
     RectangularLonLatDomain lonlatBoundingBox( const Domain& domain ) const override {
         return ProjectionImpl::lonlatBoundingBox( domain );
      }
 
-    //void stretch_LAM( eckit::Parametrisation&, PointXY );
-    //atlas::PointXY stretch_LAM() const;
     void checkvalue(const double&, const double&) const;
     double general_stretch (double&, const bool&, int&, const int&) const;
-    //atlas::PointXY stretch_LAM_gen(atlas::PointXY&);
-    //void stretch_LAM_gen(double crd[]);
-
-
 
 protected:
-    //Normalise normalise_;
-    //util::NormaliseLongitude normalise_stretch_;
 
-    // INPUT used in stretch
-    //util::Config Domainconfig;
-    //util::Config reg_grid_config;
-    //util::Config stretch_config;
-
-
-    double delta_low_; //resolution of the host model
-    double delta_high_; //resolution of the regional model (regular grid)
-    double var_ratio_; //power used for the stretching
-    double x_reg_start_; //xstart of the internal regional grid
-    double y_reg_start_; //ystart of the internal regional grid
-    double x_reg_end_; //xend of the internal regional grid
-    double y_reg_end_; ////yend of the internal regional grid
-    double startx_; //domain startx
-    double endx_; //domain endx
-    double starty_; //domain starty
-    double endy_; //domain endy
-
+    double delta_low_; ///< resolution of the host model
+    double delta_high_; ///< /< resolution of the regional model (regular grid)
+    double var_ratio_; ///< power used for the stretching
+    double x_reg_start_; ///< xstart of the internal regional grid
+    double y_reg_start_; ///< ystart of the internal regional grid
+    double x_reg_end_; ///< xend of the internal regional grid
+    double y_reg_end_; ///< yend of the internal regional grid
+    double startx_; ///< domain startx
+    double endx_; ///< domain endx
+    double starty_; ///< domain starty
+    double endy_; ///< domain endy
 
     void setup( const eckit::Parametrisation& p );
 
 private:
-    //PointXY pointxy_;
     Rotation rotation_;
 };
 

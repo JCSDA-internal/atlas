482bfd06efcec09bc4e88cb05aeed40c635d6151
mo-joshuacolclough <joshua.colclough@metoffice.gov.uk>
2026-03-05 15:11:13 +0000

    Fix 3dfgat_atms

--- src/atlas/interpolation/method/sphericalvector/SphericalVector.cc
+++ src/atlas/interpolation/method/sphericalvector/SphericalVector.cc
@@ -149,15 +149,15 @@ void SphericalVector::do_execute(const Field& sourceField, Field& targetField,
                                  Metadata&) const {
   ATLAS_TRACE("atlas::interpolation::method::SphericalVector::do_execute()");
 
-  if (targetField.size() == 0) {
-    haloExchange(sourceField);
-    return;
-  }
-
   const auto fieldType = sourceField.metadata().getString("type", "");
   if (fieldType != "vector") {
     auto metadata = Metadata();
     Method::do_execute(sourceField, targetField, metadata);
+    return;
+  }
+
+  if (targetField.size() == 0) {
+    haloExchange(sourceField);
     return;
   }
 
@@ -193,15 +193,29 @@ void SphericalVector::do_execute_adjoint(Field& sourceField,
   ATLAS_TRACE(
       "atlas::interpolation::method::SphericalVector::do_execute_adjoint()");
 
-  if (targetField.size() == 0) {
-    adjointHaloExchange(sourceField);
-    return;
-  }
+  ATLAS_TRACE("SphericalVector::do_execute_adjoint(): source '" +
+              sourceField.name() + "' => '" + targetField.name() + "' target.\n" +
+              "sizes => " + std::to_string(sourceField.size()) +
+              ", " + std::to_string(targetField.size()) + "\n");
+
+  /*
+
+  - (1) Check every PE goes down same branch...
+
+  - (2) Check each PE has the same rank field.
+
+  */
 
   const auto fieldType = sourceField.metadata().getString("type", "");
   if (fieldType != "vector") {
+    // (1)
     auto metadata = Metadata();
     Method::do_execute_adjoint(sourceField, targetField, metadata);
+    return;
+  }
+
+  if (targetField.size() == 0) {
+    adjointHaloExchange(sourceField);
     return;
   }
 
